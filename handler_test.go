813bf89b271e1f1aebbf7c862abc5fca634707b3
rangguy <ranggadwi100@gmail.com>
2024-11-13 18:05:39 +0700

    Handler

---
+++ handler_test.go
@@ -0,0 +1,25 @@
+package golang_web
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+
+	var handler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+		// logic web
+		fmt.Fprint(w, "Hello World")
+	}
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		return
+	}
+}
